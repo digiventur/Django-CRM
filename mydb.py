2f46fe3899100d6f4757787e09d0049e9f28362a
Andrew Berrios <a.i.berrios001@gmail.com>
2024-05-01 14:50:56 +1200

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,22 @@
+# Install Mysql on your computer
+# https://dev.mysql.com/downloads/installer/
+# pip install mysql
+# pip install mysql-connector
+# pip install mysql-connector-python
+
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+	host = 'localhost',
+	user = 'root',
+	passwd = 'pwd12345'
+
+	)
+
+# prepare a cursor object
+cursorObject = dataBase.cursor()
+
+# create a database
+cursorObject.execute("CREATE DATABASE kimco")
+
+print("All Done!")
